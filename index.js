4c548a6d2de3e0713ab8624ca3f7aaadc93f0a09
Dezmou <incredibledez@gmail.com>
2021-01-09 23:08:35 +0000

    init

---
+++ index.js
@@ -0,0 +1,127 @@
+
+const cardsMap = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
+].map(e => e.join())
+
+
+function htmlToElement(html) {
+  var template = document.createElement('template');
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+const sleep = async (millis) => {
+  await new Promise(r => setTimeout(r, millis))
+}
+
+const X = 0
+const Y = 1
+
+const BLANK = [255, 255, 255, 255]
+const HERO = 1
+const VILAIN = 2
+const CARD_PIOCHE = [27, 170]
+
+  ; (async () => {
+    const PXRATIO = window.devicePixelRatio
+    const canvas = await (async () => {
+      while (true) {
+        await new Promise(r => setTimeout(r, 100))
+        const canvas = document.querySelector("canvas")
+        if (canvas) return canvas
+      }
+    })()
+    console.log(canvas);
+    const ctx = canvas.getContext("2d")
+    console.log(ctx);
+    document.body.appendChild(htmlToElement(`<div>X : <span id="x_"></span> <br/>Y : <span id="y_"></span> <br/>C : <span id="c_"></span> </div>`))
+
+    const pixequal = (pix, pix2) => {
+      return (pix[0] === pix2[0] && pix[1] === pix2[1] && pix[2] === pix2[2])
+    }
+
+    const putPixel = (pix, color) => {
+      ctx.fillStyle = color
+      ctx.fillRect(pix[X] * PXRATIO, pix[Y] * PXRATIO, 1, 1)
+    }
+
+    const getPixel = (x, y) => {
+      return ctx.getImageData(x * PXRATIO, y * PXRATIO, 1, 1).data
+    }
+
+
+    const scanCard = (cardPos) => {
+      const X_STEP = 2
+      const Y_STEP = 3
+      const info = ctx.getImageData(cardPos[X] * PXRATIO, cardPos[Y] * PXRATIO, X_STEP * 8 * PXRATIO, Y_STEP * 8 * PXRATIO).data
+      const step = Math.floor(info.length / 64);
+      let map = Array.from({ length: 64 })
+      for (let i = 0; i < 64; i++) {
+        map[i] = info[i * step + 1] === 255 ? 0 : 1
+      }
+      map = map.join()
+      const i = cardsMap.findIndex(e => e === map)
+      // console.log(i + 1);
+
+      const couleur = (() => {
+        if (getPixel(61, 215)[0] === 17) { //black
+          return getPixel(55, 208)[0] === 17 ? 3 : 1;
+        } else {
+          return getPixel(49, 202)[0] === 209 ? 0 : 2
+        }
+      })()
+      console.log(couleur);
+    }
+
+    canvas.addEventListener("mousemove", (e) => {
+      document.querySelector("#x_").innerHTML = e.offsetX
+      document.querySelector("#y_").innerHTML = e.offsetY
+      const data = ctx.getImageData(e.offsetX * PXRATIO, e.offsetY * PXRATIO, 1, 1).data;
+      document.querySelector("#c_").innerHTML = data
+    })
+    const getPlayerTUrn = async () => {
+      while (true) {
+        if (pixequal(getPixel(376, 135), BLANK)) {
+          return VILAIN
+        }
+        if (pixequal(getPixel(378, 291), BLANK)) {
+          return HERO
+        }
+
+        await sleep(10)
+      }
+    }
+
+    let player = await getPlayerTUrn()
+    while (true) {
+      if (player === VILAIN) {
+
+      }
+      scanCard(CARD_PIOCHE)
+      while (true) {
+        const playerTmp = await getPlayerTUrn()
+        if (playerTmp !== player) {
+          player = playerTmp
+          break
+        }
+        await sleep(10)
+      }
+      await sleep(10)
+    }
+
+
+
+  })()
