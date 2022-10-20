b4d1148e65bcc71052ea669d326ce1432a50f263
Dezmou <incredibledez@gmail.com>
2022-10-20 13:59:04 +0200

    wip

--- index.js
+++ index.js
@@ -17,17 +17,19 @@ const cardsMap = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
 ].map(e => e.join())
 
-const field = Array.from({ length: 13 }).map((q, x) => Array.from({ length: 4 }).map((e, y) => {
-  const names = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-  const couleurs = ["h", "c", "d", 's']
-  return {
-    coord: [x, y],
-    id: `${names[x]}${couleurs[y]}`,
-    status: "UNKNOW"
-  }
-}))
+let field;
 
-console.log(field);
+const resetField = () => {
+  field = Array.from({ length: 13 }).map((q, x) => Array.from({ length: 4 }).map((e, y) => {
+    const names = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    const couleurs = ["h", "c", "d", 's']
+    return {
+      coord: [x, y],
+      id: `${names[x]}${couleurs[y]}`,
+      status: "UNKNOW"
+    }
+  }))
+}
 
 function htmlToElement(html) {
   var template = document.createElement('template');
@@ -46,152 +48,208 @@ const Y = 1
 const BLANK = [255, 255, 255, 255]
 const HERO = 1
 const VILAIN = 2
-const CARD_PIOCHE = [27, 170]
+const CARD_PIOCHE = [27, 170];
 
-  ; (async () => {
-    const PXRATIO = window.devicePixelRatio
-    const canvas = await (async () => {
-      while (true) {
-        await new Promise(r => setTimeout(r, 100))
-        const canvas = document.querySelector("canvas")
-        if (canvas) return canvas
+; (async () => {
+  const PXRATIO = window.devicePixelRatio
+  const canvas = await (async () => {
+    while (true) {
+      await new Promise(r => setTimeout(r, 100))
+      const canvas = document.querySelector("canvas")
+      if (canvas) return canvas
+    }
+  })()
+  const ctx = canvas.getContext("2d")
+  document.body.appendChild(htmlToElement(`<div>X : <span id="x_"></span> <br/>Y : <span id="y_"></span> <br/>C : <span id="c_"></span>  <br/> <button id="startGame">start tracker</button></div>`))
+  document.body.appendChild(htmlToElement(`<div>X : <span id="x_"></span> <br/>Y : <span id="y_"></span> <br/>C : <span id="c_"></span>  <br/> <button id="reset">reset</button></div>`))
+
+  const container = document.querySelector("#precont > div.gview.sbfixed > div.bcont.noth.usno")
+  // const canvas = document.querySelector("#precont > div.gview.sbfixed > div.bcont.noth.usno > canvas.noth");
+  const hud = document.createElement("canvas");
+  const hudCtx = hud.getContext("2d");
+
+  hud.width = canvas.width * PXRATIO;
+  hud.height = canvas.height * PXRATIO;
+  hud.style.width = canvas.clientWidth;
+  hud.style.height = canvas.clientHeight;
+
+  // hud.style.width = canvas.clientWidth;
+  // hud.style.height = canvas.clientHeight;
+  hud.style.position = "absolute";
+  hud.style['z-index'] = "9999";
+  hud.style['pointerEvents'] = "none";
+  hud.style['image-rendering'] = "crisp-edges";
+  container.appendChild(hud);
+  // hudCtx.fillStyle = "red";
+  // // hudCtx.rect(136,309,300,300);
+  // hudCtx.fill()
+
+
+
+  const pixequal = (pix, pix2) => {
+    return (pix[0] === pix2[0] && pix[1] === pix2[1] && pix[2] === pix2[2])
+  }
+
+  const putPixel = (pix, color) => {
+    ctx.fillStyle = color
+    ctx.fillRect(pix[X] * PXRATIO, pix[Y] * PXRATIO, 1, 1)
+  }
+
+  const getPixel = (x, y) => {
+    return ctx.getImageData(x * PXRATIO, y * PXRATIO, 1, 1).data
+  }
+
+
+  const scanCard = (cardPos) => {
+    const X_STEP = 2
+    const Y_STEP = 3
+    const info = ctx.getImageData(cardPos[X] * PXRATIO, cardPos[Y] * PXRATIO, X_STEP * 8 * PXRATIO, Y_STEP * 8 * PXRATIO).data
+    const step = Math.floor(info.length / 64);
+    let map = Array.from({ length: 64 })
+    for (let i = 0; i < 64; i++) {
+      map[i] = info[i * step + 1] === 255 ? 0 : 1
+    }
+    map = map.join()
+    const x = cardsMap.findIndex(e => e === map)
+    // console.log(i + 1);
+
+    const y = (() => {
+      if (getPixel(61, 215)[0] === 17) { //black
+        return getPixel(55, 208)[0] === 17 ? 3 : 1;
+      } else {
+        return getPixel(49, 202)[0] === 209 ? 0 : 2
       }
     })()
-    console.log(canvas);
-    const ctx = canvas.getContext("2d")
-    document.body.appendChild(htmlToElement(`<div>X : <span id="x_"></span> <br/>Y : <span id="y_"></span> <br/>C : <span id="c_"></span>  <br/> <button id="startGame">start tracker</button></div>`))
+    return field[x][y]
+  }
 
-    const hud = document.createElement("canvas");
-    const hudCtx = hud.getContext("2d");
-
-    const pixequal = (pix, pix2) => {
-      return (pix[0] === pix2[0] && pix[1] === pix2[1] && pix[2] === pix2[2])
-    }
-
-    const putPixel = (pix, color) => {
-      ctx.fillStyle = color
-      ctx.fillRect(pix[X] * PXRATIO, pix[Y] * PXRATIO, 1, 1)
-    }
-
-    const getPixel = (x, y) => {
-      return ctx.getImageData(x * PXRATIO, y * PXRATIO, 1, 1).data
-    }
-
-
-    const scanCard = (cardPos) => {
-      const X_STEP = 2
-      const Y_STEP = 3
-      const info = ctx.getImageData(cardPos[X] * PXRATIO, cardPos[Y] * PXRATIO, X_STEP * 8 * PXRATIO, Y_STEP * 8 * PXRATIO).data
-      const step = Math.floor(info.length / 64);
-      let map = Array.from({ length: 64 })
-      for (let i = 0; i < 64; i++) {
-        map[i] = info[i * step + 1] === 255 ? 0 : 1
+  canvas.addEventListener("mousemove", (e) => {
+    document.querySelector("#x_").innerHTML = e.offsetX
+    document.querySelector("#y_").innerHTML = e.offsetY
+    const data = ctx.getImageData(e.offsetX * PXRATIO, e.offsetY * PXRATIO, 1, 1).data;
+    document.querySelector("#c_").innerHTML = data
+  })
+  const getPlayerTUrn = async () => {
+    while (true) {
+      if (pixequal(getPixel(376, 135), BLANK)) {
+        return VILAIN
       }
-      map = map.join()
-      const x = cardsMap.findIndex(e => e === map)
-      // console.log(i + 1);
+      if (pixequal(getPixel(378, 291), BLANK)) {
+        return HERO
+      }
 
-      const y = (() => {
-        if (getPixel(61, 215)[0] === 17) { //black
-          return getPixel(55, 208)[0] === 17 ? 3 : 1;
+      await sleep(10)
+    }
+  }
+
+  const waitPixel = async (pos, color1) => {
+    while (true) {
+      if (getPixel(pos[0], pos[1])[0] === color1) {
+        return;
+      }
+      await sleep(10)
+    }
+  }
+
+  const getCardCoord = (x, y) => {
+    const newX = (138) + (x * 32);
+    const newY = (315) + (y * 37.5);
+    return [newX, newY];
+  }
+
+  const print = () => {
+    hudCtx.clearRect(0, 0, hud.width, hud.height);
+    for (let line of field) {
+      for (let card of line) {
+        if (card.status === "VILAIN") {
+          const coords = getCardCoord(card.coord[0], card.coord[1]);
+          hudCtx.fillStyle = `#ff0000BB`;
+          hudCtx.fillRect(coords[0], coords[1], 32, 37.5)
+        }
+        if (card.status === "REFUSED_VILAIN") {
+          const coords = getCardCoord(card.coord[0], card.coord[1]);
+          hudCtx.fillStyle = `#8a8a8aBB`;
+          hudCtx.fillRect(coords[0], coords[1], 32, 37.5)
+        }
+        if (card.status === "PROPOSE") {
+          const coords = getCardCoord(card.coord[0], card.coord[1]);
+          hudCtx.fillStyle = `#7765c4BB`;
+          hudCtx.fillRect(coords[0], coords[1], 32, 37.5)
+        }
+      }
+    }
+  }
+  let gameId = 0;
+
+  const start = async () => {
+    await (async () => {
+      const currentGameId = gameId;
+      resetField();
+      while (true) {
+        let player = await getPlayerTUrn()
+        if (currentGameId !== gameId) return;
+        const piocheBase = scanCard(CARD_PIOCHE)
+        if (player === VILAIN) {
+          while (true) {
+            if (getPixel(263, 69)[0] !== 40) {//vilain action
+              const tmpPioche = scanCard(CARD_PIOCHE)
+              if (tmpPioche === piocheBase) {
+                piocheBase.status = "REFUSED_VILAIN"
+              } else {
+                console.log("VILAIN TAKE");
+                piocheBase.status = "VILAIN"
+              }
+              break
+            }
+            if (currentGameId !== gameId) return;
+            await sleep(10)
+          }
+          console.log("VILAIN :", piocheBase);
+          print();
+          await waitPixel([378, 291], 255)// hero to play
+          if (currentGameId !== gameId) return;
         } else {
-          return getPixel(49, 202)[0] === 209 ? 0 : 2
-        }
-      })()
-      return field[x][y]
-    }
+          const pioche = scanCard(CARD_PIOCHE)
+          pioche.status = "PROPOSE"
+          print();
 
-    canvas.addEventListener("mousemove", (e) => {
-      document.querySelector("#x_").innerHTML = e.offsetX
-      document.querySelector("#y_").innerHTML = e.offsetY
-      const data = ctx.getImageData(e.offsetX * PXRATIO, e.offsetY * PXRATIO, 1, 1).data;
-      document.querySelector("#c_").innerHTML = data
-    })
-    const getPlayerTUrn = async () => {
-      while (true) {
-        if (pixequal(getPixel(376, 135), BLANK)) {
-          return VILAIN
-        }
-        if (pixequal(getPixel(378, 291), BLANK)) {
-          return HERO
-        }
+          const clickPos = await new Promise(r => {
+            const click = (e) => {
+              if (
+                (e.layerX > 21 && e.layerX < 98 && e.layerY > 165 && e.layerY < 257)
+                || (e.layerX > 643 && e.layerX < 734 && e.layerY > 149 && e.layerY < 275)
 
-        await sleep(10)
-      }
-    }
-
-    const waitPixel = async (pos, color1) => {
-      while (true) {
-        if (getPixel(pos[0], pos[1])[0] === color1) {
-          return;
-        }
-        await sleep(10)
-      }
-    }
-
-    const getCardCoord = (x, y) => {
-      const newX = (138) + (x * 32);
-      const newY = (315) + (y * 37.5);
-      return [newX, newY];
-    }
-
-    const print = () => {
-      hudCtx.clearRect(0, 0, hud.clientWidth, hud.clientHeight);
-      for (let line of field) {
-        for (let card of line) {
-          if (card.status === "VILAIN") {
-            const coords = getCardCoord(card.coord[0], card.coord[1]);
-            hudCtx.fillStyle = `#ff000055`;
-            hudCtx.fillRect(coords[0], coords[1], 32, 37.5)
+              ) {
+                canvas.removeEventListener('click', click);
+                r([
+                  e.layerX,
+                  e.layerY,
+                ]);
+              }
+            }
+            canvas.addEventListener("click", click)
+          })
+          if (currentGameId !== gameId) return;
+          pioche.status = "REFUSED_VILAIN"
+          print()
+          while (true) {
+            let tmpPlayer = await getPlayerTUrn()
+            if (tmpPlayer === VILAIN) {
+              break;
+            }
+            if (currentGameId !== gameId) return;
+            await sleep(10)
           }
         }
       }
-    }
+    })()
+    console.log("GAME ENDED");
+    resetField();
+    print();
+  }
 
-    const start = async () => {
-      const container = document.querySelector("#precont > div.gview.sbfixed > div.bcont.noth.usno")
-      const canvas = document.querySelector("#precont > div.gview.sbfixed > div.bcont.noth.usno > canvas.noth");
-      hud.width = canvas.width * PXRATIO;
-      hud.height = canvas.height * PXRATIO;
-      hud.style.width = canvas.clientWidth;
-      hud.style.height = canvas.clientHeight;
-
-      // hud.style.width = canvas.clientWidth;
-      // hud.style.height = canvas.clientHeight;
-      hud.style.position = "absolute";
-      hud.style['z-index'] = "9999";
-      hud.style['pointerEvents'] = "none";
-      hud.style['image-rendering'] = "crisp-edges";
-      container.appendChild(hud);
-      // hudCtx.fillStyle = "red";
-      // // hudCtx.rect(136,309,300,300);
-      // hudCtx.fill()
-
-
-      let player = await getPlayerTUrn()
-      const piocheBase = scanCard(CARD_PIOCHE)
-      if (player === VILAIN) {
-        while (true) {
-          if (getPixel(263, 69)[0] !== 40) {//taken
-            piocheBase.status = "VILAIN"
-            break
-          }
-          if (getPixel(378, 291) === 255) {//discard
-            piocheBase.status = "REFUSED_VILAIN"
-            break
-          }
-          await sleep(10)
-        }
-        console.log("VILAIN :", piocheBase);
-        print();
-        await waitPixel([378, 291], 255)// hero to play
-        console.log("HERO TURN NOW");
-      } else {
-
-      }
-    }
-
-    document.querySelector("#startGame").addEventListener("click", start)
-
-
-  })()
+  document.querySelector("#startGame").addEventListener("click", start)
+  document.querySelector("#reset").addEventListener("click", () => {
+    gameId += 1;
+  })
+})()
